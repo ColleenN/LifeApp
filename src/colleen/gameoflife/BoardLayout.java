7d87f4a973e7c223aafbb6cb2c6344a01c1da3c8
ColleenN <ColleenFNelson@gmail.com>
2014-12-10 21:45:17 -0800

    In the process of adding toggle between wrapped and unwrapped board.
    Signed-off-by: ColleenN <ColleenFNelson@gmail.com>

--- src/colleen/gameoflife/BoardLayout.java
+++ src/colleen/gameoflife/BoardLayout.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.GridLayout;
 
-public class BoardLayout extends GridLayout {
+public class BoardLayout extends GridLayout
+{
 
 	public BoardLayout(Context context)
 	{
@@ -25,8 +27,21 @@ public class BoardLayout extends GridLayout {
 
 	public void onLayout(boolean changed, int left, int top, int right, int bottom)
 	{
-		if(getChildCount() == 0) fillBoard();
+		if(getChildCount() == 0) 
+			setupBoard();
+		else
+		{
+			if(getRowCount() > calcNumRows())
+				trimRow();
+			if(getColumnCount() > calcNumCols())
+				trimColumn();
+			if(getRowCount() < calcNumRows())
+				addRow();
+			if(getColumnCount() < calcNumCols())
+				addColumn();
+		}
 		super.onLayout(changed, left, top, right, bottom);
+		setPadding(calcHorizPad(), calcVertPad(), 0, 0);
 	}
 	
 	public void evolve()
@@ -52,28 +67,119 @@ public class BoardLayout extends GridLayout {
 		}
 	}
 	
-	
-	
-	
-	private void fillBoard()
+	//Saves a string representation of the board
+	public String save()
 	{
-		//Calc # Columns
-		int cellW = getContext().getResources().getDimensionPixelSize(R.dimen.cell_width);
-		setColumnCount(getWidth()/cellW);
+		String state = "<board>";
+		state += "<width>" + getColumnCount() + "</width>";
+		state += "<height>" + getRowCount() + "</height>";
+		for(int i = 0; i < getChildCount(); i++)
+		{
+			if(((LifeButton)getChildAt(i)).isLive())
+			{
+				int[] coord = getCoords(i);
+				state += "<coordinate>" + coord[0] + "," + coord[1] + "</coordinate>";
+			}
+		}
+		state += "</board>";
+		return state;
+	}
+	
+	//Fills spaces on board as specified in state
+	public void initialize(String state)
+	{
+		state = state.split("<width>")[1];
+		int width = Integer.valueOf(state.split("</width>")[0]);
+		state = state.split("</width>")[1];
+		state = state.split("<height>")[1];
+		int height = Integer.valueOf(state.split("</height>")[0]);
+		state = state.split("</height>")[1];
+		setColumnCount(width);
+		setRowCount(height);
 		
-		//Calc # Rows
+		while(state.contains("<coordinate>"))
+		{
+			state = state.split("<coordinate>")[1];
+			String coordStr = state.split("</coordinate>")[0];
+			state = state.split("</coordinate>")[1];
+			
+			int[] coord = new int[2];
+			coord[0] = Integer.valueOf(coordStr.split(",")[0]);
+			coord[1] = Integer.valueOf(coordStr.split(",")[1]);
+			((LifeButton)getChildAt(getIndex(coord))).setLive(true);
+		}
+	}
+	
+	private void trimRow()
+	{
+		for(int i = 0; i < getColumnCount(); i++)
+			removeViewAt(getChildCount());
+		setRowCount(getRowCount()-1);
+	}
+	
+	private void addRow()
+	{
+		for(int i = 0; i < getColumnCount(); i++)
+			LayoutInflater.from(getContext()).inflate(R.layout.life_btn_init, this, true);
+		setRowCount(getRowCount()+1);
+	}
+	
+	private void trimColumn()
+	{
+		ArrayList<View> toGo = new ArrayList<View>();
+		for(int i = 1; i <= getRowCount(); i++)
+			toGo.add(getChildAt(getColumnCount() * i - 1));
+		for(View v : toGo)
+			removeView(v);
+		
+		setColumnCount(getColumnCount()-1);
+	}
+
+	private void addColumn()
+	{
+		for(int i = 1; i <= getRowCount(); i++)
+		{
+			View v = LayoutInflater.from(getContext()).inflate(R.layout.life_btn_init, this, false);
+			addView(v, (getColumnCount()+1) * i - 1);
+		}
+		setColumnCount(getColumnCount()+1);
+	}
+	
+	//calculates the # rows board should have
+	private int calcNumRows()
+	{
 		int cellH = getContext().getResources().getDimensionPixelSize(R.dimen.cell_height);
-		int numRows = getHeight()/cellH;
+		return getHeight()/cellH;
+	}
+	
+	//calculates the # columns board should have
+	// calculates the # of columns board should have
+	private int calcNumCols()
+	{
+		int cellW = getContext().getResources().getDimensionPixelSize(R.dimen.cell_width);
+		return getWidth()/cellW;
+	}
+	
+	private int calcVertPad()
+	{
+		int cellH = getContext().getResources().getDimensionPixelSize(R.dimen.cell_height);
+		return (getHeight() - (getRowCount()*cellH))/2;
+	}
+	
+	private int calcHorizPad()
+	{
+		int cellW = getContext().getResources().getDimensionPixelSize(R.dimen.cell_width);
+		return (getWidth() - (getColumnCount()*cellW))/2;
+	}
+	
+	private void setupBoard()
+	{
+		setColumnCount(calcNumCols());
+		int numRows = calcNumRows();
 		
 		// Populate grid
 		for(int i = 0; i < getColumnCount()*numRows; i++)
 			LayoutInflater.from(getContext()).inflate(R.layout.life_btn_init, this, true);
-		
-		int vertPad = (getHeight() - (numRows*cellH))/2;
-		int horizPad = (getWidth() - (getColumnCount()*cellW))/2;
-		
-		setPadding(horizPad, vertPad, 0, 0);
-		
 	}
 	
 	private int getNumAdjLive(int btnIndex)
--- src/colleen/gameoflife/MainActivity.java
+++ src/colleen/gameoflife/MainActivity.java
@@ -1,15 +1,16 @@
 package colleen.gameoflife;
 
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.*;
+import android.widget.Button;
 
 public class MainActivity extends Activity
 {
 	private int delay = 500; // Time between steps when in run mode
 	private Handler timer;
+	BoardLayout activeBoard;
 	Runnable mySimRunner = new Runnable()
 	{
 		public void run()
@@ -19,21 +20,23 @@ public class MainActivity extends Activity
 		}
 	};
 	
+	
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		timer = new Handler();
+		activeBoard = (BoardLayout)findViewById(R.id.gameBoard);
 	}
 	
 	public void clearBoard(View v)
 	{
-		((BoardLayout)findViewById(R.id.gameBoard)).clear();
+		activeBoard.clear();
 	}
 	
 	public void doStep(View v)
 	{
-		((BoardLayout)findViewById(R.id.gameBoard)).evolve();
+		activeBoard.evolve();
 	}
 	
 	public void toggle(View v)
@@ -47,7 +50,7 @@ public class MainActivity extends Activity
 	public void runSim(View v)
 	{
 		mySimRunner.run();
-		v.setVisibility(View.INVISIBLE);
+		v.setVisibility(View.GONE);
 		findViewById(R.id.endBtn).setVisibility(View.VISIBLE);
 		findViewById(R.id.stepBtn).setEnabled(false);
 	}
@@ -55,10 +58,34 @@ public class MainActivity extends Activity
 	public void endSim(View v)
 	{
 		timer.removeCallbacks(mySimRunner);
-		v.setVisibility(View.INVISIBLE);
+		v.setVisibility(View.GONE);
 		findViewById(R.id.runBtn).setVisibility(View.VISIBLE);
 		findViewById(R.id.stepBtn).setEnabled(true);
 	}
 	
-}	
+	public void swapType(View v)
+	{
+		BoardLayout regBoard = (BoardLayout)findViewById(R.id.gameBoard);
+		WrappedBoardLayout wrapBoard = (WrappedBoardLayout)findViewById(R.id.wrappedGameBoard);
+		if(regBoard.getVisibility() == View.VISIBLE)
+		{
+			String state = regBoard.save();
+			wrapBoard.initialize(state);
+			regBoard.setVisibility(View.GONE);
+			wrapBoard.setVisibility(View.VISIBLE);
+			((Button) v).setText(R.string.btn_label6);
+			activeBoard = wrapBoard;
+		}
+		else
+		{
+			String state = wrapBoard.save();
+			regBoard.initialize(state);
+			wrapBoard.setVisibility(View.GONE);
+			regBoard.setVisibility(View.VISIBLE);
+			((Button) v).setText(R.string.btn_label5);
+			activeBoard = regBoard;
+		}
+		activeBoard.postInvalidate();
+	}
 	
+}
